1699e21f3606376f48ef353c75f5772705d90703
jaimemendozadev <jaimemendozadev@gmail.com>
2017-07-01 12:50:59 -0700

    Implement working version of Stripe with element form
    Tutorial code uses outdated Stripe.js ver 2 code. Had
    to refactor code from Stripe docs using Stripe.js
    ver 3 code.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,16 @@
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.querySelector('.purchase_form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
+
 $(function(){
 
   var stripe = Stripe('pk_test_uRzmnFDP58RQeTEq25nKkmRy');
@@ -6,10 +19,20 @@ $(function(){
   // Custom styling can be passed to options when creating an Element.
   var style = {
     base: {
-      // Add your base input styles here. For example:
+      color: '#303238',
       fontSize: '16px',
-      lineHeight: '24px'
-    }
+      lineHeight: '48px',
+      fontSmoothing: 'antialiased',
+      '::placeholder': {
+        color: '#ccc',
+      },
+    },
+    invalid: {
+      color: '#e5424d',
+      ':focus': {
+        color: '#303238',
+      },
+    },
   };
 
   // Create an instance of the card Element
@@ -18,42 +41,34 @@ $(function(){
   // Add an instance of the card Element into the `card-element` <div>
   card.mount('#card-element');
 
+  card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = '';
+    }
+  });
+
 
   $('form').submit(function(event){
+    event.preventDefault();
+
+
     var form = $(this);
     form.find('.submit').attr('disabled', true);
 
-    stripe.createToken(card).then(function(result){
-      if(response.error){
-        form.find('.submit').attr('disabled', false);
-        $('.error_message').html(response.error.message);
-      }
-      else {
-        $('<input>').attr({
-          type: 'hidden',
-          value: response.id,
-          name: 'stripeToken'
-        }).appendTo(form);
-        form.get(0).submit();
-      }
-    });
+    stripe.createToken(card).then(function(result) {
 
-/*
-    Stripe.createToken(this, function(status, response){
-      if(response.error){
-        form.find('.submit').attr('disabled', false);
-        $('.error_message').html(response.error.message);
+      if (result.error) {
+        // Inform the user if there was an error
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to your server
+        stripeTokenHandler(result.token);
       }
-      else {
-        $('<input>').attr({
-          type: 'hidden',
-          value: response.id,
-          name: 'stripeToken'
-        }).appendTo(form);
-        form.get(0).submit();
-      }
-    });
-*/
-    event.preventDefault();
+    });    
   });
+
 });
